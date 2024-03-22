ead485f666965460e7f6ada971ebb0ae49cbdaa8
AmarnathaGowda <amarnath1413@gmail.com>
2024-03-22 18:50:34 +0530

    Project Template and Logger,Exception and Util added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'yolov7_safety_detection',
+    version= '0.0.0',
+    author= 'Amarnatha Gowda',
+    author_email= 'amarnathagowdat@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
